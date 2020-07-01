0cecc950e3f337dbd1d73c1e0ae6fdb13a7ae7af
Nikita Galanov <galanov888@gmail.com>
2020-07-01 17:33:25 +0300

    add migration, controller and views

---
+++ Controllers/HomeControllers.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Test.Models;
+using System.Linq;
+
+namespace Test.Controllers{
+    public class HomeController: Controller
+    {
+        private IRepository repository;
+        public HomeController(IRepository repo)=>
+            this.repository = repo;
+    }
+
+    public ViewResult Index(){
+        int hour = DateTime.Now.Hour;
+        ViewBag.Greeting = hour<12 ? "Good Morning" : "Good Afternoon";
+        return View("MyView");
+    }
+
+    [HttpGet]
+    public ViewResult RsvpForm =>View();
+
+    [HttpPost]
+    public ViewResult RsvpForm (GuestResponse guestResponse){
+        if (ModelState.IsValid)
+        {
+            repository.AddResponse(guestResponse);
+            return View("Thanks",guestResponse);
+        }else{
+            return View();
+        }
+    }
+
+    public ViewResult ListResponses() =>
+        View(repository.Responses.Where(r=>r.WillAttend == true));
+}
